f0aea399eae244433f71c6d405e26e8f141aeb15
matveimart1 <m.ovcharik@mail.ru>
2025-05-21 18:24:12 +0300

    Create a teachers

--- Shedule/Shedule/Program.cs
+++ Shedule/Shedule/Program.cs
@@ -11,8 +11,20 @@ namespace Shedule
     class Program
     {
         static void Main()
-        {
-            Console.WriteLine("Hello, World!");
+        {   
+            List<Lessons> StepanLessons = new List<Lessons>();
+            StepanLessons.Add(Lessons.Informatic);
+            StepanLessons.Add(Lessons.Mathematic);
+            var Stepan = new Teacher("Stepan",StepanLessons, 1);
+            Console.WriteLine(Stepan.ToString());
+            var Kirill = new Teacher("Kirill", 
+                new List<Lessons> { Lessons.Mathematic, Lessons.Informatic },
+                1);
+            Console.WriteLine(Kirill.ToString());
+            var AlexVyach = new Teacher("Aleksandr Vyacheslavovich",
+                new List<Lessons> { Lessons.Mathematic, Lessons.Informatic, Lessons.Physic },
+                10);
+            Console.WriteLine(AlexVyach.ToString());
         }
     }
 }
--- Shedule/Shedule/Teacher.cs
+++ Shedule/Shedule/Teacher.cs
@@ -8,5 +8,18 @@ namespace Shedule
 {
     public class Teacher
     {
+        public string Name { get; }
+        public List<Lessons> lessons = new List<Lessons>();
+        public int Priority;
+        public Teacher(string name,List<Lessons> _lessons,int priority) { 
+            Name = name;
+            lessons = _lessons;
+            Priority = priority;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя: {Name}\nПредметы: {string.Join(',', lessons)}\nПриоритет: {Priority}";
+        }
     }
 }
