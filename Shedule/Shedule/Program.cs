72bbf15bd348a9555216fea6e37124a46809d6de
matveimart1 <m.ovcharik@mail.ru>
2025-05-21 18:08:59 +0300

    Teacher alpha

--- Shedule/Shedule/Program.cs
+++ Shedule/Shedule/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Shedule
+{
+    public enum Lessons
+    {
+        Mathematic,
+        Physic,
+        Informatic
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
